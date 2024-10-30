79ff0479f9a6bae14b62cbe6fc650567e91b4e8b
SoJuSo <asdfg7123@gmail.com>
2024-10-30 17:31:30 +0900

    ✨feat: feed와 더불어 rss 파일까지 생성하도록 코드 수정 및 gitignore최신화

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -47,19 +47,30 @@ async function generateRSS(config, allBlogs, page = 'feed.xml') {
 
   if (publishPosts.length > 0) {
     for (const tag of Object.keys(tagData)) {
-      const filteredPosts = allBlogs.filter((post) =>
-        post.tags.map((t) => slug(t)).includes(tag)
-      )
+      const filteredPosts = allBlogs.filter((post) => post.tags.map((t) => slug(t)).includes(tag))
       const rss = generateRss(config, filteredPosts, `tags/${tag}/${page}`)
       const rssPath = path.join(outputFolder, 'tags', tag)
       mkdirSync(rssPath, { recursive: true })
       writeFileSync(path.join(rssPath, page), rss)
+      writeFileSync(path.join(rssPath, 'rss.xml'), rss)
     }
   }
 }
 
 const rss = () => {
   generateRSS(siteMetadata, allBlogs)
-  console.log('RSS feed generated...')
+  console.log(`
+    ==========================================
+    ✨ RSS Feeds Generated Successfully! ✨
+    ==========================================
+    - Main RSS Feeds:
+      - ${outputFolder}/feed.xml
+      - ${outputFolder}/rss.xml
+    
+    - Tag-specific Feeds:
+      - ${outputFolder}/tags/code
+      - ${outputFolder}/tags/features
+      - ${outputFolder}/tags/markdown
+    `)
 }
 export default rss
